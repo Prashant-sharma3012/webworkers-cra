ca79d2d8fd4326fbe14eb76f93db94514a95596a
prashant sharma <prashant.sharma@enquero.com>
2020-05-13 15:25:48 +0530

    parsing and scanning of file happening on a web worker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import XLSX from 'xlsx';
 
 // eslint-disable-next-line
 import Worker from 'worker-loader!./worker.js';
 
 function App() {
+
+  const [workbook, setWorkbook] = useState(null)
+  const [rows, setRows] = useState(null)
+
   let myWorker = new Worker();
 
-  myWorker.onmessage = function(e) {
-    console.log('Message received from worker');
-    console.log(e.data);
+  myWorker.onmessage = function (e) {
+    switch (e.data.command) {
+      case 'PARSE_EXCEL':
+        setWorkbook(e.data.workbook)
+        return
+      case 'READ_ROWS':
+        setRows(e.data.rows)
+        return
+      default:
+        break;
+    }
   }
 
   const handleFile = (t) => {
-    myWorker.postMessage(t.target.files);
+    myWorker.postMessage({ file: t.target.files, command: 'PARSE_EXCEL' });
+  }
+
+  const getRows = () => {
+    myWorker.postMessage({ workbook: workbook, sheetName: Object.keys(workbook.Sheets)[0], command: 'READ_ROWS' });
   }
 
   return (
-    <div className="App">
-      <input type="file" onChange={handleFile} />
+    <div style={{display: 'flex', flexDirection:'row', height: '100vh', justifyContent: 'center'}}>
+      <div style={{display: 'flex', alignItems: 'center'}}>
+        <input type="file" onChange={handleFile} />
+        <button onClick={getRows} disabled={!workbook}>GetRows</button>
+      </div>
     </div>
   );
 }
--- src/worker.js
+++ src/worker.js
@@ -1,17 +1,24 @@
 import XLSX from 'xlsx';
 
-onmessage = function(e) {
-  // do something async
-  myAsyncFunction(e).then(res => {
-    postMessage(res);
-  })
+onmessage = function (e) {
+
+  switch (e.data.command) {
+    case 'PARSE_EXCEL':
+      return parseExcel(e).then(res => postMessage(res))    
+    case 'READ_ROWS':
+      return readRows(e).then(res => postMessage(res))
+    default:
+      break;
+  }
+
 }
 
 // just multiplies two values and returns them
 
-const myAsyncFunction = (e) => {
-  return new Promise((s,f) => {
-    let file = e.data
+const parseExcel = (e) => {
+  return new Promise((s, f) => {
+    let file = e.data.file
+    let command = e.data.command
 
     let reader = new FileReader();
 
@@ -24,8 +31,18 @@ const myAsyncFunction = (e) => {
       var endTime = new Date();
 
       console.log("Finished at", endTime)
-      s(workbook);
-  };
-
+      s({workbook , command});
+    };
   })
+}
+
+const readRows = (e) => {
+  let { workbook, sheetName, command } = e.data
+
+  return new Promise((resolve) => {
+    console.log("Starting to Scan", new Date())
+    var rows = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], { defval: "" });
+    console.log("Scan Complete", new Date())
+    resolve({rows, command});
+  });
 }
