bbce3d818848117627b716f0cd603b613bd5d8da
prashant sharma <prashant.sharma@enquero.com>
2020-05-13 21:51:40 +0530

    update UI with loaders

--- src/components/UploadIndicator.js
+++ src/components/UploadIndicator.js
@@ -1,11 +1,18 @@
 import React, { useContext } from 'react'
 import AppContext from '../AppContext'
 import { makeStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import IconButton from '@material-ui/core/IconButton';
+import CheckCircle from '@material-ui/icons/CheckCircle';
+
 
 const useStyles = makeStyles({
   container: {
-
+    display: 'flex'
   },
+  label: {
+    marginRight: '8px'
+  }
 });
 
 
@@ -14,26 +21,37 @@ export default function UploadIndicator() {
   const classes = useStyles()
 
   const viewFile = () => {
-    
+
   }
 
   return (
-    <div className={classes.context}>
-      <div>
+    <div className={classes.container}>
+      <div className={classes.label}>
         {context.uploading.get
-          ? 'Processing File'
+          ? 'Processing'
           : ''
         }
         {
           context.uploaded.get
-          ? 'Processing Complete'
-          : ''
+            ? 'Completed'
+            : ''
         }
       </div>
       <div>
-        {context.uploaded.get && 
-          <button onClick={viewFile}>view</button>
-        }  
+        {context.uploading.get &&
+          <CircularProgress
+            color="secondary"
+            size={24}
+            thickness={4}
+          />
+        }
+      </div>
+      <div>
+        {context.uploaded.get &&
+          <IconButton style={{padding: '0'}} onClick={viewFile}>
+            <CheckCircle />
+          </IconButton>
+        }
       </div>
     </div>
   )
