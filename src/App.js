41132c5a1e47509967ff7dd033868985d73eb106
prashant sharma <prashant.sharma@enquero.com>
2020-05-13 15:02:07 +0530

    offloaded excel paersing to web worker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import XLSX from 'xlsx';
 
 // eslint-disable-next-line
 import Worker from 'worker-loader!./worker.js';
@@ -8,29 +9,18 @@ import Worker from 'worker-loader!./worker.js';
 function App() {
   let myWorker = new Worker();
 
-  myWorker.postMessage([1,2]);
-
   myWorker.onmessage = function(e) {
     console.log('Message received from worker');
     console.log(e.data);
   }
 
+  const handleFile = (t) => {
+    myWorker.postMessage(t.target.files);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={handleFile} />
     </div>
   );
 }
--- src/worker.js
+++ src/worker.js
@@ -1,3 +1,5 @@
+import XLSX from 'xlsx';
+
 onmessage = function(e) {
   // do something async
   myAsyncFunction(e).then(res => {
@@ -9,6 +11,21 @@ onmessage = function(e) {
 
 const myAsyncFunction = (e) => {
   return new Promise((s,f) => {
-    return s(e.data[0] * e.data[1]);
+    let file = e.data
+
+    let reader = new FileReader();
+
+    reader.readAsArrayBuffer(file[0]);
+
+    reader.onload = function (evnt) {
+      console.log("Processing file at", new Date())
+      var data = new Uint8Array(evnt.target.result);
+      var workbook = XLSX.read(data, { type: 'array' });
+      var endTime = new Date();
+
+      console.log("Finished at", endTime)
+      s(workbook);
+  };
+
   })
 }
